2d5287cd79fd649238b786fc80015f2e5a972a39
arikonn <nanadora202020@icloud.com>
2022-08-29 21:16:12 +0900

    add test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid? #@user.valid?がtrueを返すと成功、falseを返すと失敗
+  end
 end
