f30d05b08aeef7ff007f49d7dae80deb5d6d45d4
arikonn <nanadora202020@icloud.com>
2022-11-09 21:25:45 +0900

    917

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create8
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
